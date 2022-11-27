e1507c9124283fb0f8e3ea6c30df468c1cfbdc47
Krzysztof Napiórkowski <krzysna@gmail.com>
2022-11-27 22:07:12 +0100

    Final

---
+++ stworz_grafy.sh
@@ -0,0 +1,8 @@
+gnuplot ./Grafy/pierwsza/statyczna/pierwsza_statyczna.gnuplot
+gnuplot ./Grafy/pierwsza/dynamiczna/pierwsza_dynamiczna.gnuplot
+gnuplot ./Grafy/druga/statyczna/druga_statyczna.gnuplot
+gnuplot ./Grafy/druga/dynamiczna/druga_dynamiczna.gnuplot
+gnuplot ./Grafy/szeregowo/statyczna/szeregowo_statyczna.gnuplot
+gnuplot ./Grafy/szeregowo/dynamiczna/szeregowo_dynamiczna.gnuplot
+gnuplot ./Grafy/rownolegle/statyczna/rownolegle_statyczna.gnuplot
+gnuplot ./Grafy/rownolegle/dynamiczna/rownolegle_dynamiczna.gnuplot
